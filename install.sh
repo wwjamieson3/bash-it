644d00f5e882bfcf6b2eec519bbe885e01f2d4f0
William Jamieson <william@envision-systems.com.au>
2012-11-22 13:08:30 +1100

    removed unused files and installed envision them by default

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp $HOME/.bash_profile $HOME/.bash_profile.bak
 
 echo "Your original .bash_profile has been backed up to .bash_profile.bak"
 
-cp $HOME/.bash_it/template/bash_profile.template.bash $HOME/.bash_profile
+cp $HOME/.bash_it/template/envision.template.bash $HOME/.bash_profile
 
 echo "Copied the template .bash_profile into ~/.bash_profile, edit this file to customize bash-it"
 
